6ebba88ebe8704c4f7affa0b1982e30c069fd860
brian boersen <brian.boersen@hotmail.com>
2023-03-21 15:15:46 +0100

    misc

--- index.php
+++ index.php
@@ -8,9 +8,6 @@ require_once("lib/ingredient.php");
 
 /// INIT
 $db = new database();
-$art = new article($db->getConnection());
-$user = new user($db->getConnection());
-$ki_type = new kitchen_type($db->getConnection());
 $ingr = new ingredient($db->getConnection());
 
 
@@ -18,7 +15,7 @@ $ingr = new ingredient($db->getConnection());
 //$data = $art->selectArticle(8);
 //$data = $user->selectUser(1);
 //$data = $ki_type->selectKitchenType(1);
-$data = $ingr->selectIngredient(1);
+$data = $ingr->selectIngredients(2);
 
 /// RETURN
 echo "<pre>";
