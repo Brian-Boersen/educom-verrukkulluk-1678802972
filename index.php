dc19a272a7e131641fe2579a0f0ea2367405037c
brian boersen <brian.boersen@hotmail.com>
2023-03-21 17:02:26 +0100

    init dish_info #9

--- index.php
+++ index.php
@@ -5,17 +5,20 @@ require_once("lib/article.php");
 require_once("lib/user.php");
 require_once("lib/kitchen-type.php");
 require_once("lib/ingredient.php");
+require_once("lib/dish_info.php");
+
 
 /// INIT
 $db = new database();
 $ingr = new ingredient($db->getConnection());
-
+$dish_info = new dish_info($db->getConnection());
 
 /// VERWERK 
 //$data = $art->selectArticle(8);
 //$data = $user->selectUser(1);
 //$data = $ki_type->selectKitchenType(1);
-$data = $ingr->selectIngredients(2);
+//$data = $ingr->selectIngredients(2);
+$data = $dish_info->selectDischInfo(1);
 
 /// RETURN
 echo "<pre>";
---
+++ lib/dish_info.php
@@ -0,0 +1,53 @@
+<?php
+
+class dish_info{
+    private $connection;
+    private $user;
+
+    public function __construct($connection){
+        $this->connection = $connection;
+        $this->user = new user($connection);
+    }
+
+    public function addDishInfo($gerecht_id,$rec_type,$user_id = null,$date = null,$num_field = null, $tekst_field = null){
+        
+    }
+
+    public function deleteDishInfo(){
+        //????????????????????????????????????
+    }
+
+    public function selectDischInfo($gerecht_id,$rec_type = null){
+        
+        if($rec_type == null){
+            $sql = "SELECT * FROM gerecht_info WHERE gerecht_id = $gerecht_id";
+        }
+        else{
+            $sql = "SELECT * FROM gerecht_info WHERE gerecht_id = $gerecht_id and record_type = '$rec_type'";
+        }
+
+       
+        
+        $result = mysqli_query($this->connection,$sql);
+        $gerecht_infoData = [];
+        $userData = [];
+
+        if(mysqli_num_rows($result) > 0){
+            while($gerecht_infoRow = mysqli_fetch_array($result, MYSQLI_ASSOC) ){
+                if($gerecht_infoRow["user_id"] != ""){
+                    $userData = $this->getUser($gerecht_infoRow["user_id"]);
+                }          
+                $gerecht_infoData[] = $gerecht_infoRow + $userData;
+            }
+        }else{
+            echo "error dish_info result empty: ";
+        }
+
+
+        return $gerecht_infoData;
+    }
+
+    private function getUser($user_id){
+        return $this->user->selectUser($user_id);
+    }
+}
