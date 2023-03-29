96ece546b2989dacfc217c377a07201eac3135c7
brian boersen <brian.boersen@hotmail.com>
2023-03-29 19:57:37 +0200

    home page almost done just need a footer #13

--- assets/js/img_slide.js
+++ assets/js/img_slide.js
@@ -3,10 +3,13 @@ $(DocumentType).ready(function() {
     $("#head_background_img").vegas({
         cover:true,
         slides:  [
-            { src: "https://picsum.photos/id/1/1900/1200" },
-            { src: "https://picsum.photos/id/10/1900/1200" },
-            { src: "https://picsum.photos/id/100/1900/1200" },
-            { src: "https://picsum.photos/id/1000/1900/1200" }
+            { src: "https://picsum.photos/id/429/1900/1200" },
+            { src: "https://picsum.photos/id/312/1900/1200" },
+            { src: "https://picsum.photos/id/225/1900/1200" },
+            { src: "https://picsum.photos/id/292/1900/1200" },
+            { src: "https://picsum.photos/id/488/1900/1200" },
+            { src: "https://picsum.photos/id/493/1900/1200" },
+            { src: "https://picsum.photos/id/835/1900/1200" },
         ],
         transition: 'fade'
     })
