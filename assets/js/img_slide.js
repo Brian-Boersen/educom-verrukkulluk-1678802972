3ca0f44ab2d7dab3b7977c521ce3e7f8e0e8ffe9
brian boersen <brian.boersen@hotmail.com>
2023-03-29 11:27:52 +0200

    image sliding functionality added in the header#13

---
+++ assets/js/img_slide.js
@@ -0,0 +1,14 @@
+$(DocumentType).ready(function() {
+
+    $("#head_background_img").vegas({
+        cover:true,
+        slides:  [
+            { src: "https://picsum.photos/id/1/1900/1200" },
+            { src: "https://picsum.photos/id/10/1900/1200" },
+            { src: "https://picsum.photos/id/100/1900/1200" },
+            { src: "https://picsum.photos/id/1000/1900/1200" }
+        ],
+        transition: 'fade'
+    })
+
+})
