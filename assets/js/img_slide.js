3a4554b899da17ecd78f2d505c83fbf07700439f
brian boersen <brian.boersen@hotmail.com>
2023-04-06 15:13:40 +0200

    changed slide photos

--- assets/js/img_slide.js
+++ assets/js/img_slide.js
@@ -1,12 +1,11 @@
 $(DocumentType).ready(function() {
 const slides_reel = [
 { src: "https://picsum.photos/id/835/1900/1200" },
-{ src: "https://picsum.photos/id/312/1900/1200" },
-{ src: "https://picsum.photos/id/225/1900/1200" },
 { src: "https://picsum.photos/id/292/1900/1200" },
 { src: "https://picsum.photos/id/488/1900/1200" },
 { src: "https://picsum.photos/id/493/1900/1200" }, 
 { src: "https://picsum.photos/id/429/1900/1200" }, 
+{ src: "https://picsum.photos/id/225/1900/1200" },
 ]
 
     $("#head_background_img").vegas({
